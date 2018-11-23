e1b242d8b3c480cfd40e55cd67ff9d6f98795fc9
김성훈 <seongh7800@douzone.com>
2018-11-23 13:50:24 +0900

    폴더구조 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header, Table, Rating } from 'semantic-ui-react'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Table celled padded>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell singleLine>Evidence Rating</Table.HeaderCell>
+              <Table.HeaderCell>Effect</Table.HeaderCell>
+              <Table.HeaderCell>Efficacy</Table.HeaderCell>
+              <Table.HeaderCell>Consensus</Table.HeaderCell>
+              <Table.HeaderCell>Comments</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>
+                <Header as='h2' textAlign='center'>
+                  A
+                </Header>
+              </Table.Cell>
+              <Table.Cell singleLine>Power Output</Table.Cell>
+              <Table.Cell>
+                <Rating icon='star' defaultRating={3} maxRating={3} />
+              </Table.Cell>
+              <Table.Cell textAlign='right'>
+                80% <br />
+                <a href='#'>18 studies</a>
+              </Table.Cell>
+              <Table.Cell>
+                Creatine supplementation is the reference compound for increasing muscular creatine
+                levels; there is variability in this increase, however, with some nonresponders.
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Header as='h2' textAlign='center'>
+                  A
+                </Header>
+              </Table.Cell>
+              <Table.Cell singleLine>Weight</Table.Cell>
+              <Table.Cell>
+                <Rating icon='star' defaultRating={3} maxRating={3} />
+              </Table.Cell>
+              <Table.Cell textAlign='right'>
+                100% <br />
+                <a href='#'>65 studies</a>
+              </Table.Cell>
+              <Table.Cell>
+                Creatine is the reference compound for power improvement, with numbers from one
+                meta-analysis to assess potency
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
       </div>
     );
   }
---
+++ src/components/commons/Btn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+const Btn = () => {
+    return ( 
+        <div>
+            <Button primary>Primary</Button>
+        </div>
+    );
+}
+ 
+export default Btn;
